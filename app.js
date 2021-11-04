e3f86d7ba8a61297d511de8a00bb65a31792b9c8
azad <mohammadazad.bc@gmail.com>
2021-11-04 14:32:54 +0100

    social book

---
+++ app.js
@@ -0,0 +1,39 @@
+let settingsmenu = document.querySelector('.settings-menu')
+let darkBtn = document.getElementById('dark-btn');
+
+
+function settingsMenuToggle(){
+settingsmenu.classList.toggle('settings-menu-height')
+}
+
+darkBtn.onclick = function(){
+    darkBtn.classList.toggle('dark-btn-on')
+    document.body.classList.toggle('dark-them')
+
+    if(localStorage.getItem('them') == "light"){
+        localStorage.setItem('them', 'dark');
+    }
+    else{
+        localStorage.setItem('them', 'light');
+    }
+}
+
+
+if(localStorage.getItem('them') == "light"){
+    darkBtn.classList.remove('dark-btn-on');
+    document.body.classList.remove('dark-them')
+}
+else if(localStorage.getItem('them') == "dark"){
+    darkBtn.classList.add('dark-btn-on');
+    document.body.classList.add('dark-them')
+}
+else{
+    localStorage.setItem('them', 'light');
+}
+
+
+localStorage.setItem("them", "dark");
+localStorage.getItem('them');
+
+
+
